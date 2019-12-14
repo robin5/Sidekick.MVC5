18e02fb87494c98d94de4e77c4fd3ba7c34ff0d5
Robin Murray <robin5@pdx.edu>
2019-12-13 18:48:46 -0800

    Added initial dashboard controller and view with fake user data for show

---
+++ PEClient/Models/EvaluationInstance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PEClient.Models
+{
+    public class EvaluationInstance
+    {
+        public string Name { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+        public string Status { get; set; }
+    }
+}
