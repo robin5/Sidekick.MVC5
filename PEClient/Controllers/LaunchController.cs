0179ca6e35b9dfe673ce06c425a49a804fc5eb37
Robin Murray <robin5@pdx.edu>
2019-12-19 22:51:30 -0800

    Adds use of ViewModel in Launch controller and view

--- PEClient/Controllers/LaunchController.cs
+++ PEClient/Controllers/LaunchController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PEClient.Models;
 
 namespace PEClient.Controllers
 {
@@ -11,7 +12,34 @@ namespace PEClient.Controllers
         // GET: Launch
         public ActionResult Index()
         {
-            return View();
+            return View(new LaunchViewModel());
+        }
+
+        // POST: Launch
+        [HttpPost]
+        public ActionResult Index(LaunchViewModel request)
+        {
+            // Validate user input fields
+            if (string.IsNullOrWhiteSpace(request.LaunchName))
+            {
+                ViewBag.ErrorMessage = "Invalid submission:  Please enter a name for the launch.";
+            }
+            else if (request.StartDateTime == null)
+            {
+                ViewBag.ErrorMessage = "Invalid submission:  Please enter the start date and time of the launch.";
+            }
+            else if (request.EndDateTime == null)
+            {
+                ViewBag.ErrorMessage = "Invalid submission:  Please enter the end date and time of the launch.";
+            }
+
+            // If any error message exist
+            if (ViewBag.ErrorMessage != null)
+            {
+                return View(request);
+            }
+
+            return RedirectToAction("Index", "Dashboard");
         }
     }
 }
---
+++ PEClient/Models/LaunchViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PEClient.Models
+{
+    public class LaunchViewModel
+    {
+        private List<string> _templateNames = new List<string>();
+        public string LaunchName { get; set; }
+        public List<string> TemplateNames
+        { 
+            get { return _templateNames; }
+        }
+        public IEnumerable<SelectListItem> SelectedPeerGroups { get; set; }
+        public IEnumerable<SelectListItem> PeerGroups { get; set; }
+
+        public string StartDateTime { get; set; }
+        public string EndDateTime { get; set; }
+
+        public LaunchViewModel()
+        {
+            _templateNames.Add("Survey-1");
+            _templateNames.Add("Survey-2");
+            _templateNames.Add("Survey-3");
+            _templateNames.Add("No Questions");
+            _templateNames.Add("CTEC-227 Class Survey");
+            _templateNames.Add("Final Test");
+
+            List<SelectListItem> _peerGroups = new List<SelectListItem>();
+            _peerGroups.Add(new SelectListItem { Text = "Team #1", Value = "1" });
+            _peerGroups.Add(new SelectListItem { Text = "Team #2", Value = "2" });
+            _peerGroups.Add(new SelectListItem { Text = "Team #3", Value = "3" });
+            _peerGroups.Add(new SelectListItem { Text = "Team #4", Value = "4" });
+            _peerGroups.Add(new SelectListItem { Text = "Team #5", Value = "5" });
+            _peerGroups.Add(new SelectListItem { Text = "Team Celeb", Value = "6" });
+            PeerGroups = _peerGroups;
+
+            List<SelectListItem> _selectedPeerGroups = new List<SelectListItem>();
+            //_selectedPeerGroups.Add(new SelectListItem { Text = "Team #2", Value = "2" });
+            SelectedPeerGroups = _selectedPeerGroups;
+        }
+    }
+}
